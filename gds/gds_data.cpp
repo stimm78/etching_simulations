3525b4b1f9e061772fd5c25d37c2712147159407
Deven Tseng <dtseng@ucsb.edu>
2024-05-27 23:33:08 -0700

    fix gds parsing

--- gds/gds_data.cpp
+++ gds/gds_data.cpp
@@ -9,9 +9,9 @@ using namespace libGDSII;
 typedef vector<int> iVec;
 typedef vector<double> dVec;
 
-struct Polygon { // A Polygon is a set of vertices, located on specified layer number
+struct PolygonLayer { // PolygonLayer holds the layer number and a list of polygons on that layer
     int layer;
-    PolygonList pl;
+    PolygonList pl; // PolygonList is a vector of polygons. Each polygon contains an size 2 dVec of points.
 };
 
 
@@ -24,30 +24,31 @@ GDSIIData* readGDS(const char* gdsFileName) {
     return gdsIIData;
 }
 
-std::vector<Polygon> parseGDSPolygons(GDSIIData* gdsIIData) {
-    vector<Polygon> polygons;
+vector<PolygonLayer> parseGDSPolygons(GDSIIData* gdsIIData) {
+    vector<PolygonLayer> polygons;
 
-    iVec allLayers = gdsIIData->GetLayers();
-
-    // iterate over all layers and polygons
-    // nl = number layers, np = number polygons, nv = number vertices
-    for (int nl = 0; nl < allLayers.size(); nl++) {
-        PolygonList PolygonsOnLayerNL = gdsIIData->GetPolygons(nl);
-        for (size_t np = 0; np < PolygonsOnLayerNL.size(); np++) {
-            polygons.push_back({nl, PolygonsOnLayerNL});
-        }
+    iVec Layers = gdsIIData->Layers;
+    for (int nl = 0; nl < Layers.size(); nl++) {
+        PolygonList layerNLPolygons = gdsIIData->GetPolygons(Layers[nl]);
+        polygons.push_back({Layers[nl], layerNLPolygons});
     }
     return polygons;
 }
 
-void printPolygons(const vector<Polygon>& polygons) { // for debugging
+void printPolygons(const vector<PolygonLayer>& polygons) { // for debugging
     for (const auto& polygon : polygons) {
-        std::cout << "Layer: " << polygon.layer << "\nVertices:\n";
-        for (const auto& vertex : polygon.pl) {
-            std::cout << "(" << vertex.at(0)<< ", " << vertex.at(1) << ")\n";
+        cout << "Layer: " << polygon.layer << "\nVertices:\n";
+        for (int np = 0; np < polygon.pl.size(); np++) {
+            for (int nv = 0; nv < polygon.pl[np].size()/2; nv++) {
+                cout << "(" << polygon.pl[np][2*nv+0] << ", " << polygon.pl[np][2*nv+1] << ")\n";
+            }
         }
         std::cout << "-----\n";
     }
+}
+
+void writePLY(const vector<PolygonLayer>& polygons) {
+
 }
 
 int main(int argc, char* argv[]) {
@@ -57,7 +58,7 @@ int main(int argc, char* argv[]) {
     }
     
     GDSIIData* gdsIIData = readGDS(argv[1]);
-    vector<Polygon> polygons = parseGDSPolygons(gdsIIData);
+    vector<PolygonLayer> polygons = parseGDSPolygons(gdsIIData);
 
     printPolygons(polygons);
     delete gdsIIData; // don't leak memory
